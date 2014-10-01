4be380f9b25f457143e1cd01e3fbcfda413bf0fe
Luis Quishpi <AJT@138.4.205.235>
2014-10-01 09:36:15 +0200

    CM 2

--- ECP1.luis.quishpi/src/main/java/Clase1.java
+++ ECP1.luis.quishpi/src/main/java/Clase1.java
@@ -2,10 +2,18 @@
 public class Clase1 {
 
     private int numero1;
+    private int numero2;
     public Clase1(){
         this.numero1=0;
+        this.numero2=0;
     }
     public int getNumero1(){
         return this.numero1;
+    }
+    public int cambio1Suma(){
+        return this.numero1+this.numero2;
+    }
+    public int cambio2Resta(){
+        return this.numero1-this.numero2;
     }
 }
