04093f7ecc754e74c6d8603c3768d53309f1923e
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-01 13:36:08 +0200

    CD 8

---
+++ ECP1.luis.quishpi/src/main/java/Clase2.java
@@ -0,0 +1,11 @@
+
+public class Clase2 {
+
+    private int punto;
+    public Clase2(){
+        this.punto=0;
+    }
+    public int cd8Mover(int posicion){
+        return this.punto+posicion;
+    }
+}
